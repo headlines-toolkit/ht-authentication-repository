f06e55db4119384fb07a126fcc0ae99fb724c334
fulleni <fulleni@hotmail.com>
2025-03-24 11:33:45 +0100

    feat: initial commit
    - Added project structure
    - Setup CI/CD workflow
    - Added README.md

---
+++ lib/src/ht_authentication_repository.dart
@@ -0,0 +1,7 @@
+/// {@template ht_authentication_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtAuthenticationRepository {
+  /// {@macro ht_authentication_repository}
+  const HtAuthenticationRepository();
+}
---
+++ test/src/ht_authentication_repository_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_authentication_repository/ht_authentication_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtAuthenticationRepository', () {
+    test('can be instantiated', () {
+      expect(HtAuthenticationRepository(), isNotNull);
+    });
+  });
+}
